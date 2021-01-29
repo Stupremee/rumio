223765336da154e9ced30392e153613141fe21ff
Justus K <justus.k@protonmail.com>
2021-01-29 12:17:56 +0100

    Prepare for release

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  rustChannel = pkgs.rust-bin.nightly."2021-01-24";
+  rustChannel = pkgs.rust-bin.stable.latest;
 
   rust = rustChannel.rust.override { extensions = [ "rust-src" ]; };
 in pkgs.mkShell {
   name = "rust-shell";
-  nativeBuildInputs = with pkgs; [ rust-analyzer rust cargo-expand ];
+  nativeBuildInputs = with pkgs; [
+    rust-analyzer
+    rust
+    cargo-expand
+    cargo-release
+  ];
 }
